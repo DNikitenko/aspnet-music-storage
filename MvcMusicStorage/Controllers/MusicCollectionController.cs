71c99591c93aafae696f1ef57246c07d2ce6d41e
DNikitenko <dmitry@vladivostok.com>
2015-11-01 18:32:24 +1000

    Artists model, partial view and controller added

---
+++ MvcMusicStorage/Controllers/MusicCollectionController.cs
@@ -0,0 +1,25 @@
+﻿using MvcMusicStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStorage.Controllers
+{
+    public class MusicCollectionController : Controller
+    {
+        // GET: MusicCollection
+        public ActionResult Index()
+        {
+            var artistsList = new List<Artist>
+            {
+                new Artist { Name = "Metallica" },
+                new Artist { Name = "Amorphis" },
+                new Artist { Name = "Gogol Bordello" }
+            };
+
+            return View(artistsList.OrderBy(x => x.Name));
+        }
+    }
+}
---
+++ MvcMusicStorage/Models/Artist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStorage.Models
+{
+    /// <summary>
+    /// Represents a single performer or a musical band
+    /// </summary>
+    public class Artist
+    {
+        /// <summary>
+        /// Primary key for Artist entity
+        /// </summary>
+        [Key]
+        public int ArtistID { get; set; }
+
+        /// <summary>
+        /// Artist name
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Additional performer information
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
